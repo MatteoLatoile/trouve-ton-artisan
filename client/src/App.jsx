123def18d528b47bdcc83e77e4e59fea054500bf
MatteoPadalinoDev <me2llincontact@gmail.com>
2025-06-04 16:33:47 +0200

    add dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+      <p>Feel free to explore and modify it!</p>
+    </div>
+  );
+};
+
+export default App;
